b2c17a23f1edef6a9d6329d1d6e0ce3f270a4cb2
남영곤 <42319010+ggons@users.noreply.github.com>
2022-06-20 08:16:49 +0900

    .

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, watch, series } = require('gulp');
+const ejs = require('gulp-ejs');
+const browserSync = require('browser-sync').create();
+
+function ejsTask() {
+  return src('./src/html/index.html').pipe(ejs()).pipe(dest('dist/html'));
+}
+
+function styleTask() {
+  return src('./src/styles/**/*').pipe(dest('dist/styles'));
+}
+
+function browsersyncServe(cb) {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watchTask() {
+  watch('*.html', browsersyncReload);
+  watch(
+    ['src/html/*.html', 'src/html/**/*.ejs', 'src/styles/**/*.*'],
+    series(ejsTask, styleTask, browsersyncReload)
+  );
+}
+
+exports.default = series(ejsTask, styleTask, browsersyncServe, watchTask);
